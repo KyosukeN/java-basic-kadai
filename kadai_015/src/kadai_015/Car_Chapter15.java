1f82ac6e874b276fd8651550214b19a1cd9f6211
KyosukeN <kyosuke.nomura89@gmail.com>
2025-01-18 18:28:57 +0900

    "kadai_015"提出用のコミット

---
+++ kadai_015/src/kadai_015/Car_Chapter15.java
@@ -0,0 +1,31 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+	// フィールドの宣言と初期化
+	private int gear = 1;
+	private int speed = 10;
+
+	public void gearChange(int afterGear) {
+		int beforeGear = this.gear;
+		this.gear = afterGear;
+
+		System.out.println("ギア" + beforeGear + "から" + this.gear + "に切り替えました");
+
+	}
+
+	public void run() {
+
+		this.speed = switch (this.gear) {
+		case 1 -> 10;
+		case 2 -> 20;
+		case 3 -> 30;
+		case 4 -> 40;
+		case 5 -> 50;
+		default -> 10;
+		};
+
+		System.out.println("速度は時速" + this.speed + "kmです");
+
+	}
+}
