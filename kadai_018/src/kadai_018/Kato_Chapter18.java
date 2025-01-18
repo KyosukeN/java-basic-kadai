66d64e1297238319dacfff5bc74b358a7c3cc67c
KyosukeN <kyosuke.nomura89@gmail.com>
2025-01-18 22:53:24 +0900

    レビュー指摘修正対応。名前の出力をcommonに変更。

--- kadai_018/src/kadai_018/Kato_Chapter18.java
+++ kadai_018/src/kadai_018/Kato_Chapter18.java
@@ -6,13 +6,13 @@ abstract public class Kato_Chapter18 {
 	public String address="東京都中野区〇×";
 
 	public void commonIntroduce() {
+		System.out.println("名前は"+this.famillyName+this.givenName+"です");
 		System.out.println("住所は"+this.address+"です");
 	};
 
 	abstract public void eachIntroduce();
 
 	public void execIntroduce() {
-		System.out.println("名前は"+this.famillyName+this.givenName+"です");
 		commonIntroduce();
 		eachIntroduce();
 		System.out.println();
